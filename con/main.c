f66040574370ee65f366eb05e33aaed5f2a54484
Peter Jaeckel <peter@hapetech.de>
2020-09-07 12:48:18 +0200

    Simple Editing using conio

--- con/main.c
+++ con/main.c
@@ -6,6 +6,7 @@
 
 #include "con.h"
 
+// got up left corner clear rest of the screen
 #define clear() printf("\033[H\033[J")
 #define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
 
@@ -29,9 +30,9 @@ struct keydef keydefs[] = {
     {.spec = 0x1b5b42,.cmd = 3},
     {.spec = 0x1b5b43,.cmd = 4},
     {.spec = 0x1b5b44,.cmd = 5},
-    {.spec = 0x1b5b,.cmd = 0},         // cmd == 0 -> it's a prefix, get next char
-    {.spec = 0x1b,.cmd = 0},           // cmd == 0 -> it's a prefix, get next char
-    {.spec = 0}                        //last spec
+    {.spec = 0x1b5b,.cmd = 0},                   // cmd == 0 -> it's a prefix, get next char
+    {.spec = 0x1b,.cmd = 0},                     // cmd == 0 -> it's a prefix, get next char
+    {.spec = 0}                                  //last spec
 };
 
 
@@ -47,10 +48,13 @@ struct field {
 KEYSPEC c;
 KEYSPEC keys[MAX_KEY];
 int fld;
+int pos;
 struct field fields[MAX_FIELD] = {
-    {.x = 5,.y = 3,.label = "TEST 1",.len = 2},
-    {.x = 5,.y = 4,.label = "TEST 2",.len = 2},
-    {.x = 5,.y = 5,.label = "TEST 3",.len = 2}
+    {.x = 5,.y = 3,.label = "Vorname",.len = 30},
+    {.x = 5,.y = 4,.label = "Nachname",.len = 30},
+    {.x = 5,.y = 5,.label = "Straße",.len = 30},
+    {.x = 5,.y = 6,.label = "PLZ",.len = 5},
+    {.x = 20,.y = 6,.label = "Ort",.len = 30}
 };
 
 
@@ -58,6 +62,10 @@ void handle_key( KEYSPEC c ) {
     for( uint_t i = MAX_KEY - 1; i > 0; i-- )
         keys[i] = keys[i - 1];
     keys[0] = c;
+
+    struct field *f = fields + fld;
+    if( strlen( f->buf ) < (ulong)f->len )
+        f->buf[pos++] = ( char )c;
 }
 
 void show_data(  ) {
@@ -71,13 +79,17 @@ void show_data(  ) {
         if( f->label ) {
             gotoxy( f->x, f->y );
             printf( "%s : ", f->label );
+            printf( "%s", f->buf );
         }
     }
 
     f = fields + fld;
     assert( f->label );
-    gotoxy( ( int )( f->x + strlen( f->label ) + 3 ), f->y );
-
+    int x = f->x;
+    x += strlen( f->label );
+    x += 3;
+    x += pos;
+    gotoxy( x, f->y );
 }
 
 
@@ -86,12 +98,12 @@ int main( void ) {
     clear(  );
 
 
-    for( uint_t i = 0; i < 10; i++ ) {
+    for( uint_t i = 0; i < 80; i++ ) {
         show_data(  );
         c = con_getch(  );
+        struct keydef *ks;
 
         while( true ) {
-            struct keydef *ks;
             uint_t i = 0;
             while( true ) {
                 ks = keydefs + i;
@@ -101,16 +113,37 @@ int main( void ) {
                 i++;
             }
             if( ks->spec == 0 )
-                break;                 // 
+                break;                           // 
 
             if( ks->cmd > 0 ) {
-                printf( "CMD: %d %llx", ks->cmd, c );
-                exit( ks->cmd );
                 break;
             }
             c = c << 8 | con_getch(  );
         }
-        handle_key( c );
+        if( ks && ks->cmd ) {
+            gotoxy( 1, 2 );
+            printf( "CMD: %d %llx", ks->cmd, c );
+            switch ( ks->cmd ) {
+                case 2:
+                    fld--;
+                    pos = 0;
+                    break;
+                case 3:
+                    fld++;
+                    pos = 0;
+                    break;
+                case 4:
+                    if( pos < fields[fld].len )
+                        pos++;
+                    break;
+                case 5:
+                    if( pos > 0 )
+                        pos--;
+                    break;
+            }
+        }
+        else
+            handle_key( c );
     }
     con_exit(  );
     return 0;
